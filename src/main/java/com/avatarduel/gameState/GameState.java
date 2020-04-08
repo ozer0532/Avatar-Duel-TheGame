086a23f8c17d889c87bf386f344fb682d1aa871e
raihan_iqbal <raihanvega24@gmail.com>
2020-04-08 08:43:00 +0700

    Implementasi DrawPhase, modify gameState

--- src/main/java/com/avatarduel/gameState/GameState.java
+++ src/main/java/com/avatarduel/gameState/GameState.java
@@ -7,7 +7,7 @@ import com.avatarduel.gameManager.*;
 abstract class GameState {
     private GameManager gameManager;
 
-    public GameState(GameManager gameManager){
+    public gameState(GameManager gameManager){
         this.gameManager = gameManager;
     }
     public GameManager getGameManager(){
