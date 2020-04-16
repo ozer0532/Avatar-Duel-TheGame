6dc7f7f52dea57296feeb67aea58f1acf7501ea0
ozer0532 <ozer0532@users.noreply.github.com>
2020-04-16 14:23:16 +0700

    Updated aura to affect attack and defense values

--- src/main/java/com/avatarduel/gameState/BattlePhase.java
+++ src/main/java/com/avatarduel/gameState/BattlePhase.java
@@ -15,6 +15,7 @@ import java.util.stream.IntStream;
 // BattlePhase.java
 public class BattlePhase extends GameState implements IMouseClickSub{
     private Char selectedCard;
+    private int selectedCardIndex;
     private Player pemain;
     private RoundInfo roundInfo;
 
@@ -53,9 +54,11 @@ public class BattlePhase extends GameState implements IMouseClickSub{
                 if (info.getCharacterSlotOccupied()) {
                     System.out.println("----USING CARD-----");
                     selectedCard = gameManager.getCurrentPlayer().getPlayerArena().getCharCard(info.getIdx());
-                    if (roundInfo.getPlayedCards().contains(selectedCard)) {
+                    if (roundInfo.getPlayedCards().contains(selectedCard) || roundInfo.getCardsAttacked().contains(selectedCard)) {
                         System.out.println("----CANCEL USAGE-----");
                         selectedCard = null;
+                    } else {
+                        selectedCardIndex = info.getIdx();
                     }
                 }
             } else {
@@ -64,26 +67,37 @@ public class BattlePhase extends GameState implements IMouseClickSub{
                 //Simpan kartu ke cardsAttacked di RoundInfo
                 if (info.getCharacterSlotOccupied() && selectedCard != null) {
                     Player opp = gameManager.getOppositePlayer();
+                    Player cur = gameManager.getCurrentPlayer();
                     Char charOpp = opp.getPlayerArena().getCharCard(info.getIdx());
                     Char charCur = this.selectedCard;
-                    int attCur = charCur.getAttack();
-                    int defOpp = charOpp.getDefense();
-                    int attOpp = charOpp.getAttack();
+                    Aura auraOpp = null;
+                    Aura auraCur = null;
+                    if (cur.getPlayerArena().getSkillCard(selectedCardIndex) instanceof Aura) {
+                        auraCur = (Aura)cur.getPlayerArena().getSkillCard(selectedCardIndex);
+                    }
+                    if (opp.getPlayerArena().getSkillCard(info.getIdx()) instanceof Aura) {
+                        auraOpp = (Aura)cur.getPlayerArena().getSkillCard(selectedCardIndex);
+                    }
+
+                    int attCur = charCur.getAttack() + (auraCur != null ? auraCur.getAttack() : 0);
+                    int defOpp = charOpp.getDefense() + (auraOpp != null ? auraOpp.getDefense() : 0);
+                    int attOpp = charOpp.getAttack() + (auraOpp != null ? auraOpp.getAttack() : 0);
                     System.out.println("----ATTACKING CARD-----");
                     if (!charOpp.getIsDefense() && (attCur > attOpp)) {
                         // karakter lawan mati
                         opp.getPlayerArena().removeCharCard(info.getIdx());
                         opp.getPlayerStats().takeDamage(attCur - attOpp);
                         gameManager.addToDiscardPile(charOpp);
+                        roundInfo.addCardsAttacked(charCur);
                     }
                     else if (charOpp.getIsDefense() && (attCur > defOpp)) {
                         // karakter lawan mati
                         opp.getPlayerArena().removeCharCard(info.getIdx());
                         gameManager.addToDiscardPile(charOpp);
+                        roundInfo.addCardsAttacked(charCur);
                     } else {
                         System.out.println("----ATTACK FAILED-----");
                     }
-                    roundInfo.addCardsAttacked(charOpp);
                 }
                 selectedCard = null;
             }
